ad479e010891ea51ae4eea5eacfb6a863797f6bc
Monisha M <monisha.02.m@gmail.com>
2021-11-13 19:23:37 +0530

    Today Program

---
+++ Employee.java
@@ -0,0 +1,14 @@
+package tamilnadu.ariyalur;
+
+import tamilnadu.chennai.Office;
+
+public class Employee
+{
+	public static void main(String[] args)
+	{
+	 Office e1 = new Office();
+		e1.work();
+		//e1.giveSnacks();
+		//System.out.println(e1.ta);
+	}
+}

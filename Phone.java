fe82dae58a83385b8e227d8b8ffa247f4e0dca61
Monisha M <monisha.02.m@gmail.com>
2021-11-20 19:54:01 +0530

    Today Program

---
+++ Phone.java
@@ -0,0 +1,18 @@
+package nineties;
+
+public class Phone
+{
+public static void main(String[] args)
+{
+
+}
+public void call()
+{
+     System.out.println("Outgoing");
+}
+public void receive()
+{
+     System.out.println("Incoming");
+}
+
+}

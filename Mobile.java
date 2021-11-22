92b927a941eb4cfa337ba034f7d7fb321ecf5ea1
Monisha M <monisha.02.m@gmail.com>
2021-11-22 22:50:04 +0530

    Today Program

---
+++ Mobile.java
@@ -0,0 +1,13 @@
+package nextgen;
+
+import nineties.Phone;
+
+public class Mobile extends Phone
+{ 
+	public static void main(String[] arge)
+	{
+		Mobile vivo = new Mobile();
+		vivo.call();
+		vivo.receive();
+	}
+}

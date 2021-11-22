72d20aff25819776552af90bf5eb887e7e8afa78
Monisha M <monisha.02.m@gmail.com>
2021-11-22 22:56:23 +0530

    Today Program

---
+++ MarutiCar.java
@@ -0,0 +1,10 @@
+public abstract class MarutiCar
+{
+String logo = "S";
+int mileage = 15;
+public abstract void sit();//Method Overriding
+public void fixAirBags()
+	{
+		System.out.println(2);
+	}
+}

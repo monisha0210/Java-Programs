2036fd12b651f8f3108030ecbcd8dab2632a4699
Monisha M <monisha.02.m@gmail.com>
2021-11-28 03:58:29 -0500

    Today Program

---
+++ SmartPhone.java
@@ -0,0 +1,16 @@
+package nextgen;
+
+import nineties.Phone;
+
+public class SmartPhone extends Mobile
+{
+public int price = 10000;
+public static void main(String[] args)
+{
+SmartPhone sp = new SmartPhone();
+sp.call();
+sp.receive();
+sp.sendSMS();
+System.out.println(sp.price);
+}
+}
